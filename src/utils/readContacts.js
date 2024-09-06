d41bab973ec17a5efdc7d6f9c9e723e4d4b4de4a
SeredaOleksandr <154976460+SeredaOleksandr@users.noreply.github.com>
2024-09-06 14:02:57 +0300

    init and correct utils

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,13 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const contacts = await fs.readFile(PATH_DB); // додати ", 'utf-8'" у дужках
+    return JSON.parse(contacts); // або JSON.parse()
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+readContacts();
